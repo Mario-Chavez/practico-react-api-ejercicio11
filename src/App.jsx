8603c09febc44c8238013f0c7b01a6bf23fe391b
Mario-Chavez <marioachavez1@gmail.com>
2023-06-02 15:21:59 -0300

    se cambio el tiulo

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     return (
         <>
             <Container className="my-5 mainPage">
-                <h1 className="display-4 text-center">Practico ReactJS ejercicio 10</h1>
+                <h1 className="display-4 text-center">Practico ReactJS ejercicio 11</h1>
                 <hr />
                 <Buscador categoria={busquedaCategoria} />
                 {titulo === true ? <TituloDeBusqueda /> : ""}
